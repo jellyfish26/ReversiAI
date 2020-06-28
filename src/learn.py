e77934021a950d08d5916289b9b729e80884354e
jellyfish26 <jellyfish0162@gmail.com>
2020-06-29 04:42:31 +0900

    bug fix and implement learn save

--- src/learn.py
+++ src/learn.py
@@ -1,13 +1,16 @@
 import game_board
 import agent
 import random
+import tqdm
 
 
 class GALearn:
     def __init__(self, evolve_times):
         self.__NUMBER_INDIVIDUALS = 20  # more than 10
+        self.__NUMBER_BATTLES = 100
         self.__EVOLVE_TIMES = evolve_times
         self.__now_generation = []
+        self.__progress_bar = None
         for number in range(0, self.__NUMBER_INDIVIDUALS):
             self.__now_generation.append([agent.GABoardAgent(), 0])
             self.__now_generation[number][0].set_random_evaluation_board()
@@ -15,11 +18,11 @@ class GALearn:
     # calc expectation value
     def __battle_random_agent(self):
         for index in range(0, self.__NUMBER_INDIVIDUALS):
-            print(index)
-            for times in range(0, 100):
+            for times in range(0, self.__NUMBER_BATTLES):
                 temp = agent.RandomAgent()
                 game = game_board.GameBoard(self.__now_generation[index][0], temp)
                 game.game_start()
+                self.__progress_bar.update(1)
                 if game.check_game_end() == -1:
                     self.__now_generation[index][1] += 1
 
@@ -29,7 +32,7 @@ class GALearn:
     def __update_generation(self):
         self.__battle_random_agent()
         self.__generation_sort()
-        ret = [[self.__now_generation[0], 0], [self.__now_generation[1], 0]]
+        ret = [[self.__now_generation[0][0], 0], [self.__now_generation[1][0], 0]]
         for times in range(2, self.__NUMBER_INDIVIDUALS):
             select_elite = self.__now_generation[random.randint(0, 1)][0]
             select_another = self.__now_generation[random.randint(2, self.__NUMBER_INDIVIDUALS - 1)][0]
@@ -42,8 +45,13 @@ class GALearn:
                 ret.append([select_elite.normal_mutation(), 0])
             else:
                 ret.append([select_elite.plus_mutation(), 0])
+        self.__now_generation = ret
 
-    def start(self):
-        for times in range(0, self.__EVOLVE_TIMES):
+    def start(self, file_path, save_interval):
+        for times in range(1, self.__EVOLVE_TIMES + 1):
+            self.__progress_bar = tqdm.tqdm(total=self.__NUMBER_BATTLES * self.__NUMBER_INDIVIDUALS)
+            self.__progress_bar.set_description('learning at generation ' + str(times))
             self.__update_generation()
-            print(times + 1, self.__now_generation[0][1], sep=' ')
+            if times % save_interval == 0:
+                self.__now_generation[0][0].save_evaluation_board(file_path + str(times))
+            self.__progress_bar.close()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import learn
 
 if __name__ == '__main__':
-    now = learn.GALearn(200)
-    now.start()
+    now = learn.GALearn(1000)
+    now.start("../data/GA-", 100)
